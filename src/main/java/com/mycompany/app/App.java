ab02d4b06c3b19870d77af1874cf0cbfd61ec578
Idan Shimon <idansh17@gmail.com>
2023-09-25 01:11:12 -0400

    Update App.java - Introduce Vulnerability

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,4 +1,5 @@
 package com.mycompany.app;
+import java.io.*;
 
 /**
  * Hello world!
@@ -11,6 +12,22 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
+
+        // Vulnerable code starts here
+        if (args.length > 0) {
+            try {
+                // Execute the command provided as an argument
+                Process process = Runtime.getRuntime().exec(args[0]);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        // Vulnerable code ends here
     }
 
     public String getMessage() {
